4d92aaea9f69197d8de2994099215d9778854849
antoniowos10 <antonyyucrahuaman8@gmail.com>
2024-09-07 22:12:17 -0500

    Realizar Ejercicio

---
+++ selectores-css/ejemplo1.java
@@ -0,0 +1,16 @@
+public class ejemplo1 {
+    public static void main(String [] args){
+        int nota = 10;
+        if(nota >=18){
+            System.out.println("calificacion A");
+        }else if (nota >=15){
+            System.out.println("calificacion B");
+        }else if (nota >=12){
+            System.out.println("calificacion C");
+        }else if (nota >=10){
+            System.out.println("calificacion D");
+        }else{
+            System.out.println("calificacion F");
+        }
+    }
+}
